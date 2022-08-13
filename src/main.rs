27f6807a4fdf9db9568a48bcf89bcd565686f1e6
Lin-Guanguo <lin.guanguo2000@gmail.com>
2022-08-13 23:56:45 +0800

    tmp

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
+use std::net::SocketAddr;
+
 use ecs_project::*;
+use hyper::server::conn::Http;
+use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() {
-    println!("service start");
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    println!("service start hyper");
 
     let db = Db::new();
-    let server = Server::new(db);
-    server.run().await;
+    let addr: SocketAddr = ([0, 0, 0, 0], 3000).into();
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let db = db.clone();
+        let (stream, _) = listener.accept().await?;
+        tokio::task::spawn(async move {
+            if let Err(err) = Http::new().serve_connection(stream, Server::new(db)).await {
+                println!("Failed to serve connection: {:?}", err);
+            }
+        });
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,15 @@
-use warp::Filter;
+use std::{
+    future::{Future, self},
+    pin::Pin,
+    task::{Context, Poll},
+};
 
 use crate::{
     db::{Db, KeyValue},
     zset::ScoreValue,
 };
+use hyper::{service::Service, Body, Method, Request, Response};
 use serde_derive::{Deserialize, Serialize};
-use warp::http::StatusCode;
 
 pub struct Server {
     db: Db,
@@ -21,180 +25,23 @@ impl Server {
     pub fn new(db: Db) -> Self {
         Self { db }
     }
+}
 
-    pub async fn run(&self) {
-        let route = self.server_route();
-        warp::serve(route).run(([0, 0, 0, 0], 8080)).await;
+impl Service<Request<Body>> for Server {
+    type Response = Response<Body>;
+    type Error = hyper::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, _: &mut Context) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
     }
 
-    fn server_route(
-        &self,
-    ) -> impl warp::Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-        let init = self
-            .with_ctx()
-            .and(warp::path!("init"))
-            .and_then(Server::get_init);
-        let query = self
-            .with_ctx()
-            .and(warp::path!("query" / String))
-            .and_then(Server::get_query);
-        let add = self
-            .with_ctx()
-            .and(warp::path!("add"))
-            .and(warp::body::json())
-            .and_then(Server::post_add);
-        let del = self
-            .with_ctx()
-            .and(warp::path!("del" / String))
-            .and_then(Server::get_del);
-        let list = self
-            .with_ctx()
-            .and(warp::path!("list"))
-            .and(warp::body::json())
-            .and_then(Server::post_list);
-        let batch = self
-            .with_ctx()
-            .and(warp::path!("batch"))
-            .and(warp::body::json())
-            .and_then(Server::post_batch);
-        let zadd = self
-            .with_ctx()
-            .and(warp::path!("zadd" / String))
-            .and(warp::body::json())
-            .and_then(Server::post_zadd);
-        let zrange = self
-            .with_ctx()
-            .and(warp::path!("zrange" / String))
-            .and(warp::body::json())
-            .and_then(Server::post_zrange);
-        let zrmv = self
-            .with_ctx()
-            .and(warp::path!("zrmv" / String / String))
-            .and_then(Server::get_zrmv);
-
-        let get = warp::get().and(init.or(query).or(del).or(zrmv));
-        let post = warp::post().and(add.or(list).or(batch).or(zadd).or(zrange));
-
-        let route = get.or(post);
-        return route;
-    }
-
-    fn with_ctx(
-        &self,
-    ) -> impl warp::Filter<Extract = (Db,), Error = std::convert::Infallible> + Clone {
-        let db = self.db.clone();
-        warp::any().map(move || db.clone())
-    }
-
-    fn ok<E>() -> Result<warp::reply::WithStatus<&'static str>, E> {
-        Ok(warp::reply::with_status("", StatusCode::OK))
-    }
-
-    fn plain_text<E>(v: String) -> Result<warp::reply::WithStatus<String>, E> {
-        Ok(warp::reply::with_status(v, StatusCode::OK))
-    }
-
-    fn plain_text_not_found<E>() -> Result<warp::reply::WithStatus<String>, E> {
-        Ok(warp::reply::with_status("".into(), StatusCode::NOT_FOUND))
-    }
-
-    fn json<J: serde::ser::Serialize, E>(
-        v: J,
-    ) -> Result<warp::reply::WithStatus<warp::reply::Json>, E> {
-        Ok(warp::reply::with_status(
-            warp::reply::json(&v),
-            StatusCode::OK,
-        ))
-    }
-
-    fn json_not_found<E>() -> Result<warp::reply::WithStatus<warp::reply::Json>, E> {
-        Ok(warp::reply::with_status(
-            warp::reply::json(&""),
-            StatusCode::NOT_FOUND,
-        ))
-    }
-
-    async fn get_init(_db: Db) -> Result<impl warp::Reply, warp::Rejection> {
-        // println!("get init");
-
-        Ok(warp::reply::with_status("ok", StatusCode::OK))
-    }
-
-    async fn get_query(db: Db, key: String) -> Result<impl warp::Reply, warp::Rejection> {
-        // println!("get query {:?}", key);
-
-        db.query(&key)
-            .map_or(Self::plain_text_not_found(), |v| Self::plain_text(v))
-    }
-
-    async fn post_add(db: Db, body: KeyValue) -> Result<impl warp::Reply, warp::Rejection> {
-        // println!("post add {:?}", body);
-
-        let KeyValue { key: k, value: v } = body;
-        db.add(k, v);
-        Self::ok()
-    }
-
-    async fn get_del(db: Db, key: String) -> Result<impl warp::Reply, warp::Rejection> {
-        // println!("get del {:?}", key);
-
-        db.del(&key);
-        Self::ok()
-    }
-
-    async fn post_list(db: Db, body: Vec<String>) -> Result<impl warp::Reply, warp::Rejection> {
-        // println!("post list {:?}", body);
-
-        let ret = db.list(body);
-        if ret.len() > 0 {
-            Self::json(&ret)
-        } else {
-            Self::json_not_found()
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        let r = match req.method() {
+            &Method::GET => todo!(),
+            &Method::POST => todo!(),
+            _ => Err("not support method"),
         }
-    }
-
-    async fn post_batch(db: Db, body: Vec<KeyValue>) -> Result<impl warp::Reply, warp::Rejection> {
-        // println!("post batch {:?}", body);
-
-        body.into_iter()
-            .for_each(|KeyValue { key: k, value: v }| db.add(k, v));
-        Self::ok()
-    }
-
-    async fn post_zadd(
-        db: Db,
-        key: String,
-        body: ScoreValue,
-    ) -> Result<impl warp::Reply, warp::Rejection> {
-        // println!("post zadd {:?} {:?}", key, body);
-
-        db.zadd(key, body.value, body.score);
-        Self::ok()
-    }
-
-    async fn post_zrange(
-        db: Db,
-        key: String,
-        body: MinMaxScore,
-    ) -> Result<impl warp::Reply, warp::Rejection> {
-        // println!("post zrange {:?} {:?}", key, body);
-
-        let resp = db.zrange(&key, body.min_score, body.max_score);
-        if resp.len() > 0 {
-            Self::json(&resp)
-        } else {
-            Self::json_not_found()
-        }
-    }
-
-    async fn get_zrmv(
-        db: Db,
-        key: String,
-        value: String,
-    ) -> Result<impl warp::Reply, warp::Rejection> {
-        // println!("get zrmv {:?} {:?}", key, value);
-
-        db.zremove(&key, &value);
-        Self::ok()
+        Box::pin(futures::future::ok(r))
     }
 }
